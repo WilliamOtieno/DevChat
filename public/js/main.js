e0ce3f20179f0e7bce72dde149598ac592795ff2
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-05-21 00:00:06 +0300

    refactor

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages')
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 
 
 const { username, room } = Qs.parse(location.search, {
@@ -9,6 +11,12 @@ const { username, room } = Qs.parse(location.search, {
 const socket = io();
 
 socket.emit('joinRoom', { username, room })
+
+
+socket.on('roomUsers', ({ room, users }) => {
+    outputRoomName(room);
+    outputUsers(users);
+})
 
 
 socket.on('message', message => {
@@ -36,4 +44,14 @@ function outputMessage(message) {
         ${message.text}
     </p>`;
     document.querySelector('.chat-messages').appendChild(div);
+}
+
+function outputRoomName(room) {
+    roomName.innerText = room;
+}
+
+function outputUsers(users) {
+    userList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join()}
+    `
 }
