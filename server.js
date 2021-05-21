c5b93ebb0288b50edaca33c11623511b4e01bf41
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-05-21 06:44:58 +0300

    remove bug

--- server.js
+++ server.js
@@ -26,12 +26,14 @@ io.on('connection', socket => {
         socket.emit('message', formatMessage(botName, 'Welcome to DevChat!'))
         socket.broadcast.to(user.room).emit('message', formatMessage(botName,`${user.username} has joined the chat`));
 
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+
     });
 
-    io.to(user.room).emit('roomUsers', {
-        room: user.room,
-        users: getRoomUsers(user.room)
-    })
+    
 
     socket.on('chatMessage', (msg) => {
         const user = getCurrentUser(socket.id)
